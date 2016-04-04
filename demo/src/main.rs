441c706e4f769d2e36e081b9e0b1388ffc95a89c
Benjamin Fox <benjaminf@intergen.co.nz>
2016-04-04 11:06:06 -0700

    Removed dead launch.json. Fixed initial demo state

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,6 +1,6 @@
 
-//mod demo0;
-//use demo0::main as demo_main;
+mod demo0;
+use demo0::main as demo_main;
 
 //mod demo1;
 //use demo1::main as demo_main;
@@ -17,8 +17,8 @@
 //mod demo5;
 //use demo5::main as demo_main;
 
-mod demo6;
-use demo6::main as demo_main;
+//mod demo6;
+//use demo6::main as demo_main;
 
 fn main() {
     demo_main();
